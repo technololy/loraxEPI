9b96311d9c3a2f7fdeb1eef91dfb11cbe1cbcd62
Mark Roberts <mroberts@loraxcompliance.com>
2024-03-18 15:50:12 +0000

    Add project files.

---
+++ InterviewTestMid/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace InterviewTestMid
+{
+    internal class Logger
+    {
+
+        public void WriteLogMessage(string LogMessage)
+        {
+            if (string.IsNullOrEmpty(LogMessage))
+                throw new ArgumentException(LogMessage);
+
+            Debug.WriteLine(LogMessage);
+        }
+
+        public void WriteErrorMessage(Exception Ex)
+        {
+            if (Ex == null)
+                throw new ArgumentException("Ex");
+
+            Debug.WriteLine($"Error recieved: {Ex.Message}");
+            Debug.WriteLine($"{Ex.StackTrace}");
+        }
+    }
+}
---
+++ InterviewTestMid/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace InterviewTestMid
+{
+    internal class Program
+    {
+        public Program()
+        {
+            DoWork();
+        }
+
+        private void DoWork()
+        {
+            Logger Log = new ();
+            Log.WriteLogMessage("Doing some JSON tasks...");
+
+            //Do JSON tasks here.
+
+            Log.WriteLogMessage("Finished doing some JSON tasks.");
+        }
+    }
+}
